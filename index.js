0735126f85a12818f03d5817a063432d749066a1
Mike de Geofroy <59496187+MikeDeGeofroy@users.noreply.github.com>
2022-08-26 00:04:45 +0300

    finished bot (almost)

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
 // Telegram bot
 
 import fetch from 'node-fetch';
-import mongoose from 'mongoose';
-import { Bot, InlineKeyboard, session, InputFile } from "grammy";
+
+// Grammy.js imports
+
+import { Bot, InlineKeyboard, session, InputFile, Context } from "grammy";
 import { hydrate } from "@grammyjs/hydrate";
-import {
-  conversations,
-  createConversation,
-} from "@grammyjs/conversations";
-import nodemailer from 'nodemailer'
+import { conversations, createConversation } from "@grammyjs/conversations";
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'mikedegeofroy',
-    pass: 'pvjfzcnqfnhxfixs'
-  }
-});
+// MongoDB models import
 
+import models from './models.js'
+
+let users = models.users
+let orders = models.orders
+
+// MongoDB setup
+
+import mongoose from 'mongoose';
 let dbURI = 'mongodb://127.0.0.1:27017/telegram-bot-api'
-
-var OrderSchema = new mongoose.Schema({
-  posting_number: { type: String, unique: true },
-  date: {type: String, required: false },
-  products: {type: Array, required: false }
-}, { collection: "orders", required: false });
-
-var PersonSchema = new mongoose.Schema({
-  user_id: { type: Number, unique: true, required: false },
-  username: { type: String, required: false },
-  first_name: { type: String, required: false },
-  last_name: { type: String, required: false },
-  email: { type: String, required: false },
-  phone: { type: String, required: false },
-  orders: { type: Array, required: false }
-}, { collection: "people" });
 
 mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, error => {
   if (!error) {
@@ -43,6 +27,214 @@ mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true }, err
     console.log(error)
   }
 })
+
+// Email sending options
+
+import nodemailer from 'nodemailer'
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mikedegeofroy',
+    pass: 'pvjfzcnqfnhxfixs'
+  }
+});
+
+// Bot Setup
+
+const bot = new Bot('5596272178:AAH1yClgbyUJHgZhGbyVa5Kqb8w1rEk4pjU');
+
+// MongoDB Grammy.js integration
+
+bot.use(session({
+  initial: () => ({})
+}));
+
+// Hydate setup
+
+bot.use(hydrate());
+
+// Conversatinons Setup
+
+bot.use(conversations());
+
+// This bot is split up into conversations, where each step of the process enters the user into a conversation, most often, they enter with a "inline keyboard", inline keyboards return callback queries, all the callback queries can be seen from line []
+
+// Verify phone number
+
+// Send the code to the users phone number
+
+async function sendVerificationCode(phone) {
+
+  var code = Math.floor(1000 + Math.random() * 9000);
+
+  let url = 'https://lcab.sms-uslugi.ru/json/v1.0/sms/send/text';
+
+  phone = phone.join('')
+
+  console.log(phone)
+
+  let options = {
+    method: 'POST',
+    headers: {
+      'X-Token': 'nast26by0ig4dubf3xu332gido4m3pksqmf63o6i9ad8y9f4vue5yidjgn7p257i',
+      'Content-Type': 'application/json'
+    },
+    body: `{"messages":[{"recipient": "${phone}","recipientType":"recipient","id":"string","source":"Flowerium","timeout":3600,"text":"Ваш код: ${code}"}],"validate":false,"tags":["2022","Регистрация"],"timeZone":"Europe/Moscow"}`
+  };
+
+  await fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+
+  return code
+
+}
+
+bot.use(createConversation(sendVerificationCode));
+
+async function verifyCode(conversation, ctx) {
+
+  console.log("Entered verify code")
+
+  let user = await users.findOne({ "user_id": ctx.update.callback_query.from.id })
+
+  console.log(user)
+
+  let { message } = await conversation.wait();
+
+  if(parseInt(message.text) == user.verify_code){
+    let badge = user.header
+
+    await bot.api.editMessageText(badge.chat.id, badge.message_id, "Подтвержден ✅")
+
+    await users.findOneAndUpdate({ "user_id": ctx.from.id }, { "verified": true }, { upsert: true, new: true }, (err, result) => {
+      if (err) {
+        // console.log(err);
+      } else {
+        console.log(result);
+      }
+    }).clone()
+  }
+
+
+}
+
+bot.use(createConversation(verifyCode));
+
+bot.callbackQuery("verifyCode", async (ctx) => {
+  await ctx.conversation.enter("verifyCode");
+});
+
+async function phone(conversation, ctx) {
+
+  let user = await users.findOne({ "user_id": ctx.update.callback_query.from.id })
+
+  let badge = user.header
+
+  try{
+    await bot.api.editMessageText(badge.chat.id, badge.message_id, "Напишите ваш номер телефона")
+  } catch {
+    console.log("lol")
+  }
+
+  const { message } = await conversation.wait();
+
+  console.log(message)
+
+  const code = await conversation.external( () => sendVerificationCode(message.text.match(/\d/g)))
+
+  await users.findOneAndUpdate({ "user_id": ctx.from.id }, { "verify_code": code, "phone": message.text.match(/\d/g).join('') }, { upsert: true, new: true }, (err, result) => {
+    if (err) {
+      // console.log(err);
+    } else {
+      console.log(result);
+    }
+  }).clone()
+
+  const inlineKeyboard = new InlineKeyboard().text('Ввести код', 'verifyCode')
+
+  await bot.api.editMessageText(badge.chat.id, badge.message_id, `Код отправлен на номер ${message.text}`, { reply_markup: inlineKeyboard })
+
+
+}
+
+bot.use(createConversation(phone));
+
+bot.callbackQuery("phone", async (ctx) => {
+  await ctx.conversation.enter("phone");
+});
+
+// Send files to user email
+
+async function email(conversation, ctx) {
+  const header = await ctx.reply("Напишите вашу эл. почту");
+
+  await users.findOneAndUpdate({ "user_id": ctx.from.id }, { header: header }, { upsert: true, new: true }, (err, result) => {
+    if (err) {
+      // console.log(err);
+    } else {
+      console.log(result);
+    }
+  }).clone()
+
+  const { message } = await conversation.wait();
+
+  await bot.api.deleteMessage(message.from.id, message.message_id)
+
+  // Adding the email to the user
+
+  users.findOneAndUpdate({ "user_id": message.from.id }, { "email": message.text }, (err, result) => {
+    if (err) {
+      // console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+
+  const inlineKeyboard = new InlineKeyboard().text('Ошиблись почтой', 'email').text('Продтвердить', 'phone');
+
+
+  await header.editText("Прислали! Чтобы получить остальные мателриалы, подтвердите ваш номер телефона", {
+    reply_markup: inlineKeyboard,
+  }).then(() => {
+
+    // These are the mail options
+
+    var mailOptions = {
+      from: 'mike@degeofroy.com',
+      to: message.text,
+      subject: 'Plant Info',
+      text: 'Some fun files that you should enjoy.',
+      attachments: [
+        {
+          filename: 'Plants.pdf',
+          path: 'telegram-bot-tz.pdf'
+        },
+      ]
+    };
+
+    // Sending the mail
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  });
+
+}
+
+bot.use(createConversation(email));
+
+
+bot.callbackQuery("email", async (ctx) => {
+  await ctx.conversation.enter("email");
+});
+
+// Verify Order Number
 
 async function getPostingNumber(id) {
 
@@ -57,8 +249,6 @@ async function getPostingNumber(id) {
     },
     body: '{"dir":"DESC","limit":500,"offset":0,"filter":{"status":"delivered"},"translit":true,"with":{"analytics_data":true,"financial_data":false}}'
   };
-
-  let orders = mongoose.model("orders", OrderSchema);
 
   await fetch(url, options)
     .then(res => res.json())
@@ -94,196 +284,103 @@ async function getPostingNumber(id) {
 
 }
 
-const bot = new Bot('5596272178:AAH1yClgbyUJHgZhGbyVa5Kqb8w1rEk4pjU');
+async function orderNumber(conversation, ctx) {
 
-bot.use(hydrate());
+  console.log("entered convo")
 
-bot.use(session({ initial: () => ({}) }));
-
-bot.use(conversations());
-
-bot.use(createConversation(email));
-
-bot.use(createConversation(order_number));
-
-async function order_number(conversation, ctx) {
   const statusMessage = await ctx.reply("Введите номер заказа");
 
   const { message } = await conversation.wait();
 
   await statusMessage.editText("Секундочку...").then(async () => {
 
-    let people = mongoose.model("people", PersonSchema);
+    await conversation.external(async () => {
 
-    let order_number = /\d{8}-\d{4}/m.exec(message.text)
+      let order_number = /\d{8}-\d{4}/m.exec(message.text)
 
-    let posting_number = undefined
+      let posting_number = undefined
 
-    if (order_number != null) {
-      posting_number = await getPostingNumber(order_number[0])
-      console.log(posting_number)
-    }
-
-    await people.findOneAndUpdate({ "user_id": message.from.id }, { $addToSet: { orders: posting_number }, }, { upsert: true, new: true }, (err, result) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        console.log(result);
+      if (order_number != null) {
+        posting_number = await getPostingNumber(order_number[0])
+        console.log(posting_number)
       }
-    }).clone()
 
-    if (posting_number) {
+      await users.findOneAndUpdate({ "user_id": message.from.id }, { $addToSet: { orders: posting_number }, }, { upsert: true, new: true }, (err, result) => {
+        if (err) {
+          // console.log(err);
+        } else {
+          console.log(result);
+        }
+      }).clone()
 
-      let inlineKeyboard1 = new InlineKeyboard().url(
-        "Channel",
-        "https://t.me/+VJCqx58vHsiOW0FB",
-      )
+      if (posting_number) {
 
-      const inlineKeyboard2 = new InlineKeyboard().text('Отправить', 'email');
+        let inlineKeyboard1 = new InlineKeyboard().url(
+          "Channel",
+          "https://t.me/+VJCqx58vHsiOW0FB",
+        ).text('Отправить', 'email');
 
-      await statusMessage.editText("Подтвержден ✅")
+        await statusMessage.editText("Подтвержден ✅")
 
-      const badge = await ctx.replyWithPhoto(new InputFile("background.jpeg"), {
-        caption: "The invitation will expire in 30 minutes.", reply_markup: inlineKeyboard1
-      })
+        const badge = await ctx.replyWithPhoto(new InputFile("background.jpeg"), {
+          caption: "Приглашение истечет через 30 минут, также, мы можем выслать вам дополнительный контент на почту.", reply_markup: inlineKeyboard1
+        })
 
-      setTimeout(async () => {
-        await ctx.api.deleteMessage(ctx.chat.id, statusMessage.message_id);
-        await ctx.api.deleteMessage(ctx.chat.id, message.message_id)
-      }, 250)
+        setTimeout(async () => {
+          await ctx.api.deleteMessage(ctx.chat.id, statusMessage.message_id);
+          await ctx.api.deleteMessage(ctx.chat.id, message.message_id)
+        }, 250)
 
-      setTimeout(async () => {
-        inlineKeyboard1 = new InlineKeyboard().text(
-          "Expired"
-        )
-        // await inlineKeyboard1.editText('bitch', 'email')
-        await bot.api.editMessageReplyMarkup(badge.chat.id, badge.message_id, { reply_markup: inlineKeyboard1 })
-      }, 30000)
+        setTimeout(async () => {
+          inlineKeyboard1 = new InlineKeyboard().text(
+            "Expired"
+          ).text('Книги', 'email');
+          // await inlineKeyboard1.editText('bitch', 'email')
+          await bot.api.editMessageReplyMarkup(badge.chat.id, badge.message_id, { reply_markup: inlineKeyboard1 })
+        }, 1800000)
 
-      await ctx.reply("Мы отправим ваши книги на эл. почту!", {
-        reply_markup: inlineKeyboard2,
-      })
+      } else {
+        const inlineKeyboard = new InlineKeyboard().text('Try again', 'order_number');
 
-    } else {
-      const inlineKeyboard = new InlineKeyboard().text('Try again', 'order_number');
+        await ctx.reply("It looks like your order number is incorrect, please try again.", {
+          reply_markup: inlineKeyboard
+        })
 
-      await ctx.reply("It looks like your order number is incorrect, please try again.", {
-        reply_markup: inlineKeyboard
-      })
-
-    }
+      }
+    });
 
   });
+
 }
 
+bot.use(createConversation(orderNumber));
+
+bot.callbackQuery("orderNumber", async (ctx) => {
+  await ctx.conversation.enter("order_number");
+});
+
+// Start bot
 
 bot.command("start", async (ctx) => {
 
-  let people = mongoose.model("people", PersonSchema);
-    
-  await people.findOneAndUpdate({ "user_id": ctx.from.id }, { "username": ctx.from.username, "first_name": ctx.from.first_name, "last_name": ctx.from.last_name }, { upsert: true, new: true }, (err, result) => {
+  await ctx.conversation.exit()
+
+  await users.findOneAndUpdate({ "user_id": ctx.from.id }, { "username": ctx.from.username, "first_name": ctx.from.first_name, "last_name": ctx.from.last_name }, { upsert: true, new: true }, (err, result) => {
     if (err) {
       // console.log(err);
     } else {
       console.log(result);
     }
   }).clone()
-  
-  await ctx.conversation.enter("order_number");
-  
+
+  await ctx.conversation.enter("orderNumber");
 
   await ctx.deleteMessage();
 
 });
 
-
-// bot.hears(/\d{8}-\d{4}/m, async (ctx) => {
-// Explicit usage
-// ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.update.message.from.first_name}`)
-// /\d{8}-\d{4}/m
-
-// lets pretend this outputs true await getPostingNumber(ctx.update.message.text)
-
-
-// })
-
-async function email(conversation, ctx) {
-  await ctx.reply("Напишите вашу эл. почту");
-
-  const { message } = await conversation.wait();
-
-  let people = mongoose.model("people", PersonSchema);
-
-  people.findOneAndUpdate({ "user_id": message.from.id }, { "email": message.text }, (err, result) => {
-    if (err) {
-      // console.log(err);
-    } else {
-      console.log(result);
-    }
-  });
-
-  const inlineKeyboard = new InlineKeyboard().url(
-    "Find out more",
-    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  ).text('Resend Email', 'email');
-
-  await ctx.reply("Sent!", {
-    reply_markup: inlineKeyboard,
-  }).then(() => {
-    var mailOptions = {
-      from: 'mike@degeofroy.com',
-      to: message.text,
-      subject: 'Plant Info',
-      text: 'Some fun files that you should enjoy.',
-      attachments: [
-        {
-          filename: 'Plants.pdf',
-          path: 'telegram-bot-tz.pdf'
-        },
-      ]
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-  });
-}
-
-bot.callbackQuery("email", async (ctx) => {
-  await ctx.conversation.enter("email");
-});
-
-
-bot.callbackQuery("order_number", async (ctx) => {
-  await ctx.conversation.enter("order_number");
-});
-
-// await ctx.conversation.enter("order_number");
-
 bot.catch((error) => {
-  console.log(error)
+  console.log(error.stack)
 })
 
 bot.start();
-
-// transporter.sendMail(mailOptions, function (error, info) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
-
-// ctx.reply('Fuck you!', { reply_markup: { force_reply: true } })
-
-// transporter.sendMail(mailOptions, function (error, info) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-//   });
---
+++ models.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+var OrderSchema = new mongoose.Schema({
+  posting_number: { type: String, unique: true },
+  date: { type: String, required: false },
+  products: { type: Array, required: false }
+}, { collection: "orders", required: false });
+
+var UserSchema = new mongoose.Schema({
+  user_id: { type: String, unique: true,required: false },
+  username: { type: String, required: false },
+  first_name: { type: String, required: false },
+  last_name: { type: String, required: false },
+  email: { type: String, required: false },
+  phone: { type: String, required: false },
+  orders: { type: Array, required: false },
+  verify_code: { type: Number, required: false },
+  verified: { type: Boolean, required: false },
+  header: { type: Object, required: false }
+}, { collection: "users" });
+
+let orders = mongoose.model("orders", OrderSchema);
+let users = mongoose.model("users", UserSchema);
+
+export default { orders, users };
+// module.exports = users;
